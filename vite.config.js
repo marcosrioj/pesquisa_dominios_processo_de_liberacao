a631f693a993b480f1f7bc8e029e399539b134ee
Marcos Lima <marcosrioj@gmail.com>
2026-01-20 21:16:21 -0800

    Github pages added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/pesquisa_dominios_processo_de_liberacao/',
   server: {
     host: true,
     port: 5173
